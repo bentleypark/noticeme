0572bc86f5c2d7e4e2ceebb3747595148fd921bc
Bentley Park <junggil.park@sovoro.kr>
2020-11-02 17:29:08 +0900

    modify homefragment(#8)

--- app/src/main/java/com/project/noticeme/ui/home/HomeFragment.kt
+++ app/src/main/java/com/project/noticeme/ui/home/HomeFragment.kt
@@ -5,12 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.project.noticeme.R
 import com.project.noticeme.common.base.ViewBindingHolder
 import com.project.noticeme.common.base.ViewBindingHolderImpl
+import com.project.noticeme.data.room.ConsumableEntity
 import com.project.noticeme.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,7 +21,8 @@ class HomeFragment : Fragment(),
     ViewBindingHolder<FragmentHomeBinding> by ViewBindingHolderImpl() {
 
     private val viewModel: HomeViewModel by viewModels()
-    private lateinit var listAdapter: ConsumableListAdapter
+    private var consumableList = mutableListOf<ConsumableEntity>()
+    private val listAdapter = ConsumableListAdapter(consumableList)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,24 +33,39 @@ class HomeFragment : Fragment(),
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setUpView()
+
         viewModel.consumableList.observe(
             viewLifecycleOwner, {
 
-                listAdapter = ConsumableListAdapter(it)
-
-                val size = resources.getDimensionPixelSize(R.dimen.material_item_size)
-                binding!!.rvList.apply {
-                    adapter = listAdapter
-                    layoutManager =
-                        LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
-                    setHasFixedSize(true)
-                    addItemDecoration(SpaceDecoration(size))
-                }
+//                binding!!.apply {
+//                    rvList.isVisible = false
+//                    ivGuideMsg.isVisible = false
+//                }
+//
+//                if (it.isNotEmpty()) {
+//                    binding.rvList.isVisible = true
+//                    consumableList = it.toMutableList()
+//                    listAdapter.addAll(consumableList)
+//                } else {
+//                    binding.ivGuideMsg.isVisible = true
+//                }
             }
         )
 
         binding!!.btnAdd.setOnClickListener {
             findNavController().navigate(R.id.action_homeFragment_to_addConsumableFragment)
+        }
+    }
+
+    private fun setUpView() {
+        val size = resources.getDimensionPixelSize(R.dimen.material_item_size)
+        binding!!.rvList.apply {
+            adapter = listAdapter
+            layoutManager =
+                LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+            setHasFixedSize(true)
+            addItemDecoration(SpaceDecoration(size))
         }
     }
 }
