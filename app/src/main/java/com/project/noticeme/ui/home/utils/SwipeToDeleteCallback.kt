f626379236445655873a083c16118c0bc9287c21
Bentley Park <junggil.park@sovoro.kr>
2020-11-05 15:33:45 +0900

    [app][detail] develop userConsumableDetialFragment layout

--- app/src/main/java/com/project/noticeme/ui/home/utils/SwipeToDeleteCallback.kt
+++ app/src/main/java/com/project/noticeme/ui/home/utils/SwipeToDeleteCallback.kt
@@ -14,7 +14,7 @@ abstract class SwipeToDeleteCallback(context: Context) : ItemTouchHelper.SimpleC
     private val intrinsicWidth = deleteIcon!!.intrinsicWidth
     private val intrinsicHeight = deleteIcon!!.intrinsicHeight
     private val background = ColorDrawable()
-    private val backgroundColor = Color.parseColor("#f44336")
+    private val backgroundColor = Color.parseColor("#629fa8")
     private val clearPaint = Paint().apply { xfermode = PorterDuffXfermode(PorterDuff.Mode.CLEAR) }
 
 
