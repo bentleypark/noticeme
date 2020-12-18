47a05074386c1e2ca34e76fea27ac921340036d4
Bentley Park <junggil.park@sovoro.kr>
2020-12-19 00:29:16 +0900

    버전코드 변경

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -2,6 +2,6 @@ object Config {
     const val compileSdkVersion = 30
     const val minSdkVersion = 23
     const val targetSdkVersion = 30
-    const val versionCode = 1
+    const val versionCode = 2
     const val versionName = "1.0.1"
 }
