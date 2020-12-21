fc188a1c6298e5ff5abbfa20024dd2db504fa0ae
Bentley Park <junggil.park@sovoro.kr>
2020-12-21 12:22:37 +0900

    bump versionName to 1.0.2, versionCode to 3

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -1,7 +1,7 @@
 object Config {
     const val compileSdkVersion = 30
-    const val minSdkVersion = 23
+    const val minSdkVersion = 21
     const val targetSdkVersion = 30
-    const val versionCode = 2
-    const val versionName = "1.0.1"
+    const val versionCode = 3
+    const val versionName = "1.0.2"
 }
