33aad326d4e0791634bb5ed057282a160af7c090
Bentley Park <junggil.park@sovoro.kr>
2020-12-26 11:18:22 +0900

    bump to gradle-6.8-rc-4, versionName 1.0.4, versionCode 5

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -2,6 +2,6 @@ object Config {
     const val compileSdkVersion = 30
     const val minSdkVersion = 21
     const val targetSdkVersion = 30
-    const val versionCode = 4
-    const val versionName = "1.0.3"
+    const val versionCode = 5
+    const val versionName = "1.0.4"
 }
