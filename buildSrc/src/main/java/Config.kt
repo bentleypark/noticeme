c27b5344ed503839b84595f2a2486a9b22b7a46e
Bentley Park <junggil.park@sovoro.kr>
2021-01-05 00:23:35 +0900

    update version info && kotlin version

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -2,6 +2,6 @@ object Config {
     const val compileSdkVersion = 30
     const val minSdkVersion = 21
     const val targetSdkVersion = 30
-    const val versionCode = 5
-    const val versionName = "1.0.4"
+    const val versionCode = 6
+    const val versionName = "1.0.5"
 }
