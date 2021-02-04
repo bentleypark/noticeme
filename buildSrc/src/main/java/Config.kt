8ba870a29f023947e13ac2c31bf05fa6dcb89372
Bentley Park <junggil.park@sovoro.kr>
2021-02-04 15:07:50 +0900

    bump to app version 1.0.6

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -3,5 +3,5 @@ object Config {
     const val minSdkVersion = 21
     const val targetSdkVersion = 30
     const val versionCode = 6
-    const val versionName = "1.0.5"
+    const val versionName = "1.0.6"
 }
