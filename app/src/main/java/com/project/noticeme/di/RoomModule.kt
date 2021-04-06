ab3ac7e7fa622a22b6764adcb7bf1f5f3e6bdc03
Bentley Park <junggil.park@sovoro.kr>
2021-04-06 09:46:32 +0900

    local db migration 작업

--- app/src/main/java/com/project/noticeme/di/RoomModule.kt
+++ app/src/main/java/com/project/noticeme/di/RoomModule.kt
@@ -35,10 +35,10 @@ class RoomModule {
 
     private val migration_1_2 = object : Migration(1, 2) {
         override fun migrate(database: SupportSQLiteDatabase) {
-            database.execSQL("CREATE TABLE consumables_new (id INTEGER NOT NULL, title TEXT NOT NULL, imageTitle TEXT NOT NULL, category TEXT NOT NULL, duration LONG NOT NULL, PRIMARY KEY(id))")
-            database.execSQL("INSERT INTO consumables_new (id, title, imageTitle, category, duration) SELECT id, title, '', category, duration FROM consumables")
-            database.execSQL("DROP TABLE consumables")
-            database.execSQL("ALTER TABLE consumables_new RENAME TO consumables")
+//            database.execSQL("CREATE TABLE consumables_new (id INTEGER NOT NULL, title TEXT NOT NULL, imageTitle TEXT NOT NULL, category TEXT NOT NULL, duration LONG NOT NULL, PRIMARY KEY(id))")
+//            database.execSQL("INSERT INTO consumables_new (id, title, imageTitle, category, duration) SELECT id, title, '', category, duration FROM consumables")
+//            database.execSQL("DROP TABLE consumables")
+//            database.execSQL("ALTER TABLE consumables_new RENAME TO consumables")
 
             database.execSQL("CREATE TABLE userConsumables_new (id INTEGER NOT NULL, title TEXT NOT NULL, imageTitle TEXT NOT NULL, category TEXT NOT NULL, duration LONG NOT NULL, startDate LONG NOT NULL, endDate LONG NOT NULL, priority INTEGER NOT NULL, PRIMARY KEY(id))")
             database.execSQL("INSERT INTO userConsumables_new (id, title, imageTitle, category, duration, startDate, endDate, priority) SELECT id, title, '', category, duration, startDate, endDate, priority FROM userConsumables")
