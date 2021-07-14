ab90f050f41c4ec686a98ae203724743dff8b700
Bentley <bentley@cleaninglab.co.kr>
2021-07-14 10:22:14 +0900

    dependency version upgrade

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,20 +1,20 @@
 object Versions {
 
     // Class Path for Plugin
-    const val buildGradleVersion = "4.1.3"
+    const val buildGradleVersion = "4.2.2"
     const val googleServiceVersion = "4.3.5"
     const val sentryGradleVersion = "1.7.36"
-    const val r8Version = "2.2.60"
+    const val r8Version = "2.2.64"
     const val androidJunit5Version = "1.6.2.0"
     const val androidMavenGradleVersion = "2.1"
-    const val detektGradleVersion = "1.16.0"
-    const val navSafArgPluginVersion = "2.3.4"
+    const val detektGradleVersion = "1.17.1"
+    const val navSafArgPluginVersion = "2.3.5"
 
     // Kotlin
-    const val kotlinVersion = "1.4.32"
+    const val kotlinVersion = "1.5.21"
 
     // Coroutines
-    const val androidVersion = "1.4.3"
+    const val androidVersion = "1.5.1"
 
     // RxJava
     const val rxJavaVersion = "2.2.21"
@@ -25,11 +25,11 @@ object Versions {
     // AndroidX
     const val appCompatVersion = "1.2.0"
     const val constraintLayoutVersion = "2.0.4"
-    const val coreKtxVersion = "1.3.2"
+    const val coreKtxVersion = "1.6.0"
     const val legacySupportVersion = "1.0.0"
-    const val activityVersion = "1.2.2"
-    const val fragmentVersion = "1.3.2"
-    const val recyclerviewVersion = "1.1.0"
+    const val activityVersion = "1.2.3"
+    const val fragmentVersion = "1.3.5"
+    const val recyclerviewVersion = "1.2.0"
     const val viewpager2Version = "1.1.0-alpha01"
     const val swipeRefreshLayoutVersion = "1.1.0"
     const val databindingVersion = "4.1.2"
@@ -37,8 +37,8 @@ object Versions {
     // Jetpack
     const val workVersion = "2.4.0"
     const val lifeCycleVersion = "2.3.1"
-    const val roomVersion = "2.2.6"
-    const val navigationVersion = "2.3.4"
+    const val roomVersion = "2.3.0"
+    const val navigationVersion = "2.3.5"
     const val startupVersion = "1.0.0"
 
     const val testVersion = "1.3.0"
@@ -46,8 +46,8 @@ object Versions {
     const val junitExtVersion = "1.1.2"
 
     // Google
-    const val materialVersion = "1.3.0"
-    const val gsonVersion = "2.8.6"
+    const val materialVersion = "1.4.0"
+    const val gsonVersion = "2.8.7"
     const val firebaseAnalyticsVersion = "17.5.0"
     const val playCoreVersion = "1.8.1"
 
@@ -66,18 +66,18 @@ object Versions {
     // Debug
     const val timberVersion = "4.7.1"
     const val stethoVersion = "1.5.1"
-    const val sentryVersion = "4.3.0"
+    const val sentryVersion = "5.0.1"
 
     // Klint
     const val klintVersion = "0.41.0"
 
     // Hilt
-    const val hiltVersion = "2.33-beta"
+    const val hiltVersion = "2.37"
     const val hiltViewModel = "1.0.0-alpha02"
 
     // Admob
-    const val admobVersion = "20.0.0"
+    const val admobVersion = "20.2.0"
 
     // Coil
-    const val coilVersion = "1.1.1"
+    const val coilVersion = "1.3.0"
 }
